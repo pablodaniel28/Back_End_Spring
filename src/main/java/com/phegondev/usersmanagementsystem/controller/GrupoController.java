0111482dd3f51b0b608b40b15e598e867670a499
YOFRANCITO <maisterloderyt@gmail.com>
2024-06-14 20:34:10 -0400

    detalles

--- src/main/java/com/phegondev/usersmanagementsystem/controller/GrupoController.java
+++ src/main/java/com/phegondev/usersmanagementsystem/controller/GrupoController.java
@@ -1,6 +1,5 @@
 package com.phegondev.usersmanagementsystem.controller;
 
-
 import com.phegondev.usersmanagementsystem.entity.Grupo;
 import com.phegondev.usersmanagementsystem.service.GrupoService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/carreras")
+@RequestMapping("/grupos")
 public class GrupoController {
 
     @Autowired
@@ -21,7 +20,6 @@ public class GrupoController {
     public List<Grupo> getAllGrupos() {
         return grupoService.getAllGrupos();
     }
-
 
     @GetMapping("/{id}")
     public ResponseEntity<Grupo> getGrupoById(@PathVariable Integer id) {
@@ -34,19 +32,73 @@ public class GrupoController {
         return grupoService.createGrupo(grupo);
     }
 
-//    @PutMapping("/{id}")
-//    public ResponseEntity<Carrera> updateCarrera(@PathVariable Integer id, @RequestBody Carrera carreraDetails) {
-//        Optional<Carrera> carrera = carreraService.updateCarrera(id, carreraDetails);
-//        return carrera.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    @PutMapping("/{id}")
+    public ResponseEntity<Grupo> updateGrupo(@PathVariable Integer id, @RequestBody Grupo grupoDetails) {
+        Optional<Grupo> grupo = grupoService.updateGrupo(id, grupoDetails);
+        return grupo.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteGrupo(@PathVariable Integer id) {
+        if (grupoService.getGrupoById(id).isPresent()) {
+            grupoService.deleteGrupo(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
+
+
+//package com.phegondev.usersmanagementsystem.controller;
+//
+//
+//import com.phegondev.usersmanagementsystem.entity.Grupo;
+//import com.phegondev.usersmanagementsystem.service.GrupoService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.List;
+//import java.util.Optional;
+//
+//@RestController
+//@RequestMapping("/carreras")
+//public class GrupoController {
+//
+//    @Autowired
+//    private GrupoService grupoService;
+//
+//    @GetMapping
+//    public List<Grupo> getAllGrupos() {
+//        return grupoService.getAllGrupos();
 //    }
 //
-//    @DeleteMapping("/{id}")
-//    public ResponseEntity<Void> deleteCarrera(@PathVariable Integer id) {
-//        if (carreraService.getCarreraById(id).isPresent()) {
-//            carreraService.deleteCarrera(id);
-//            return ResponseEntity.noContent().build();
-//        } else {
-//            return ResponseEntity.notFound().build();
-//        }
+//
+//    @GetMapping("/{id}")
+//    public ResponseEntity<Grupo> getGrupoById(@PathVariable Integer id) {
+//        Optional<Grupo> grupo = grupoService.getGrupoById(id);
+//        return grupo.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
 //    }
-}
+//
+//    @PostMapping
+//    public Grupo createGrupo(@RequestBody Grupo grupo) {
+//        return grupoService.createGrupo(grupo);
+//    }
+//
+////    @PutMapping("/{id}")
+////    public ResponseEntity<Carrera> updateCarrera(@PathVariable Integer id, @RequestBody Carrera carreraDetails) {
+////        Optional<Carrera> carrera = carreraService.updateCarrera(id, carreraDetails);
+////        return carrera.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+////    }
+////
+////    @DeleteMapping("/{id}")
+////    public ResponseEntity<Void> deleteCarrera(@PathVariable Integer id) {
+////        if (carreraService.getCarreraById(id).isPresent()) {
+////            carreraService.deleteCarrera(id);
+////            return ResponseEntity.noContent().build();
+////        } else {
+////            return ResponseEntity.notFound().build();
+////        }
+////    }
+//}
