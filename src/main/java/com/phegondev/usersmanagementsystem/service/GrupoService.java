39da1a78a40c04e5f2763ac45af3dd0e328f6538
Castedo_Daniel <117131469+pablodaniel28@users.noreply.github.com>
2024-06-15 08:46:59 -0400

    grupo controles

--- src/main/java/com/phegondev/usersmanagementsystem/service/GrupoService.java
+++ src/main/java/com/phegondev/usersmanagementsystem/service/GrupoService.java
@@ -96,7 +96,6 @@ import com.phegondev.usersmanagementsystem.repository.GestionRepository;
 import com.phegondev.usersmanagementsystem.repository.MateriaRepository;
 import com.phegondev.usersmanagementsystem.repository.UsersRepo;
 import com.phegondev.usersmanagementsystem.repository.SistemaacademicoRepository;
-import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
